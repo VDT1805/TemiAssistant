78ca5e270625f16e00c9595f8297f8975eff6761
VDT1805 <136230459+VDT1805@users.noreply.github.com>
2024-07-12 09:01:26 +0700

    Delete TemiFoodDelivery directory

--- TemiFoodDelivery/app/src/main/java/com/ibsystem/temifooddelivery/navigation/graph/Graph.kt
+++
@@ -1,7 +0,0 @@
-package com.ibsystem.temifooddelivery.navigation.graph
-
-object Graph {
-    const val ROOT = "root_graph"
-    const val MAIN = "main_graph"
-    const val DETAILS = "details_graph"
-}
--- TemiFoodDelivery/app/src/main/java/com/ibsystem/temifooddelivery/navigation/screen/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.ibsystem.temifooddelivery.navigation.screen
-
-sealed class Screen(val route: String) {
-    object OrderListScreen : Screen("order_list_screen")
-    object CustomerScreen : Screen("customer_screen")
-
-}
